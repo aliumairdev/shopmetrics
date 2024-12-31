0c71358ab258d17dfcb21d0b9e22a793a598cd1d
aliumairdev <aliumair.dev@gmail.com>
2025-01-01 02:28:05 +0500

    Refactor migration files to improve formatting of index and foreign key definitions

--- db/migrate/20241231211026_create_refer_referral_codes.refer.rb
+++ db/migrate/20241231211026_create_refer_referral_codes.refer.rb
@@ -3,7 +3,7 @@ class CreateReferReferralCodes < ActiveRecord::Migration[6.1]
   def change
     create_table :refer_referral_codes do |t|
       t.belongs_to :referrer, polymorphic: true, null: false
-      t.string :code, null: false, index: { unique: true }
+      t.string :code, null: false, index: {unique: true}
 
       t.timestamps
     end
